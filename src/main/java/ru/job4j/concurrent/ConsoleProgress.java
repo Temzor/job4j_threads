38fa137340b66e93b33e3c3b5dc8056fceb140cd
Капустин Дмитрий <onewaykd@gmail.com>
2022-08-07 20:06:10 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent;
+
+import java.util.List;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        List<Character> symbols = List.of('-', '\\', '|', '/', '-', '\\', '|', '/');
+        while (!Thread.currentThread().isInterrupted()) {
+            for (Character symbol : symbols) {
+                System.out.print("\r Loading " + symbol);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+    }
+}
