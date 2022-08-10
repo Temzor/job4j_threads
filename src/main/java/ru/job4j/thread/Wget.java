114cf01b69865d10ab6bd488535f3c606d586012
d.kapustin <onewaykd@gmail.com>
2022-08-10 16:12:41 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -7,63 +7,69 @@ import java.net.URL;
 
 public class Wget implements Runnable {
     private final String url;
+    private final String file;
     private final int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String file, int speed) {
         this.url = url;
+        this.file = file;
         this.speed = speed;
     }
 
     @Override
     public void run() {
-        String outputFile = url.split("/")[url.split("/").length - 1];
+        /* Скачать файл*/
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("out →" + outputFile)) {
-            byte[] dataBuffer = new byte[speed];
+             FileOutputStream out = new FileOutputStream(file)) {
+            byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            long currentTime = 0;
-            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                if (currentTime < 1000) {
-                    try {
-                        Thread.sleep(1000 - currentTime);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
+            long bytesWrite = 0;
+            long startTime = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                out.write(dataBuffer, 0, bytesRead); /*bytesRead - Это кол-во прочтенных за раз байт*/
+                bytesWrite += bytesRead;
+                if (bytesWrite >= speed) {
+                    long deltaTime = System.currentTimeMillis() - startTime;
+                    if (deltaTime < 1000) {
+                        Thread.sleep(1000 - deltaTime);
                     }
+                    startTime = System.currentTimeMillis();
+                    bytesWrite = 0;
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+            Thread.currentThread().interrupt();
         }
     }
 
+    public static void validateArgs(String[] args) {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("""
+                    Invalid arguments, please correct parameters: \s
+                     args[0] -> internet url
+                     args[1] -> download speed Bytes pes 1 second
+                     args[2] -> name file"""
+            );
+        }
+    }
 
     public static void main(String[] args) throws InterruptedException {
-        if (!checkArgs(args)) {
-            return;
-        }
+        validateArgs(args);
         String url = args[0];
-        int speed = Integer.parseInt(args[1]);
         System.out.println("Download: " + url);
-        System.out.println("Download speed: " + speed + "kByte/sec");
-        Thread thread = new Thread(new Wget(url, speed));
+        String file = args[1];
+        System.out.println("File name " + file);
+        int speed = Integer.parseInt(args[2]);
+        System.out.println("Download speed: " + speed + "Byte/sec");
+        Thread thread = new Thread(new Wget(url, file, speed));
         long start = System.currentTimeMillis();
         thread.start();
         thread.join();
         System.out.println("Download: ok. time="
                 + (System.currentTimeMillis() - start)
                 + " ms.");
-    }
-
-    private static boolean checkArgs(String[] args) {
-        boolean result = args.length >= 3;
-        if (!result) {
-            System.out.println("""
-                    Usage correct format for run *.jar -->
-                     Wget <url> <speed>, where:
-                     <url>          - internet url;
-                     <speed>        - download speed kBytes pes 1 second.""");
-        }
-        return result;
     }
 }
