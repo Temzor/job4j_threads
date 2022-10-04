7ec3fa23fe63a61639a74990c2060c0a66fddd02
d.kapustin <onewaykd@gmail.com>
2022-10-04 10:14:34 +0300

    4. ThreadSafe динамический список [#1105 #348520]

--- src/test/java/ru/job4j/synch/SingleLockListTest.java
+++ src/test/java/ru/job4j/synch/SingleLockListTest.java
@@ -23,17 +23,4 @@ public class SingleLockListTest {
         list.iterator().forEachRemaining(result::add);
         assertThat(result).contains(1, 2);
     }
-
-    @Test
-    public void get() throws InterruptedException {
-        SingleLockList<Integer> list = new SingleLockList<>(new ArrayList<>());
-        Thread first = new Thread(() -> list.add(1));
-        Thread second = new Thread(() -> list.add(2));
-        first.start();
-        second.start();
-        first.join();
-        second.join();
-        assertThat(1).isEqualTo(list.get(0));
-        assertThat(2).isEqualTo(list.get(1));
-    }
 }
