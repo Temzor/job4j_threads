5c02ca7dc6be10c891253d64b287acc271aefdfb
d.kapustin <onewaykd@gmail.com>
2022-10-25 09:58:40 +0300

    0. CAS - операции [#6859 #354494]

--- src/test/java/ru/job4j/cas/CASCountTest.java
+++ src/test/java/ru/job4j/cas/CASCountTest.java
@@ -23,17 +23,18 @@ class CASCountTest {
     }
 
     @Test
-    public void whenNoJoin() {
+    public void whenExecute2ThreadThen2() throws InterruptedException {
         CASCount count = new CASCount();
-        Thread first = new Thread(() -> {
-            count.increment();
-            count.increment();
-            count.increment();
-        });
-        Thread second = new Thread(count::increment);
+        var first = new Thread(count::increment);
+        var second = new Thread(count::increment);
+        /* Запускаем нити. */
         first.start();
         second.start();
-        assertThat(count.get()).isEqualTo(0);
+        /* Заставляем главную нить дождаться выполнения наших нитей. */
+        first.join();
+        second.join();
+        /* Проверяем результат. */
+        assertThat(count.get()).isEqualTo(2);
     }
 
     @Test
@@ -49,5 +50,31 @@ class CASCountTest {
         first.join();
         second.start();
         assertThat(count.get()).isEqualTo(3);
+    }
+    @Test
+    void whenIncreaseIn2ThreadsWillSumCountsOfThemWithThreadSafe() {
+        CASCount casCount = new CASCount();
+        Thread countOne = new Thread(
+                () -> {
+                    for (int i = 0; i < 2; i++) {
+                        casCount.increment();
+                    }
+                }
+        );
+        Thread countTwo = new Thread(
+                () -> {
+                    for (int i = 0; i < 2; i++) {
+                        casCount.increment();
+                    }
+                }
+        );
+        countOne.start();
+        countTwo.start();
+        countOne.interrupt();
+        countTwo.interrupt();
+        assertThat(4).isEqualTo(casCount.get());
+        casCount.increment();
+        casCount.increment();
+        assertThat(6).isEqualTo(casCount.get());
     }
 }
