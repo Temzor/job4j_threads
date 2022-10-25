116788b9708e750531200b55f62e5f225c74d855
d.kapustin <onewaykd@gmail.com>
2022-10-25 10:03:48 +0300

    0. CAS - операции [#6859 #354494]

--- src/test/java/ru/job4j/cas/CASCountTest.java
+++ src/test/java/ru/job4j/cas/CASCountTest.java
@@ -5,6 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class CASCountTest {
+
+    @Test
+    public void whenJoin() throws InterruptedException {
+        CASCount count = new CASCount();
+        Thread first = new Thread(() -> {
+            count.increment();
+            count.increment();
+            count.increment();
+        });
+        Thread second = new Thread(count::increment);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get()).isEqualTo(4);
+    }
+
     @Test
     public void whenExecute2ThreadThen2() throws InterruptedException {
         CASCount count = new CASCount();
@@ -33,32 +50,5 @@ class CASCountTest {
         first.join();
         second.start();
         assertThat(count.get()).isEqualTo(3);
-    }
-
-    @Test
-    void whenIncreaseIn2ThreadsWillSumCountsOfThemWithThreadSafe() {
-        CASCount casCount = new CASCount();
-        Thread countOne = new Thread(
-                () -> {
-                    for (int i = 0; i < 2; i++) {
-                        casCount.increment();
-                    }
-                }
-        );
-        Thread countTwo = new Thread(
-                () -> {
-                    for (int i = 0; i < 2; i++) {
-                        casCount.increment();
-                    }
-                }
-        );
-        countOne.start();
-        countTwo.start();
-        countOne.interrupt();
-        countTwo.interrupt();
-        assertThat(4).isEqualTo(casCount.get());
-        casCount.increment();
-        casCount.increment();
-        assertThat(6).isEqualTo(casCount.get());
     }
 }
